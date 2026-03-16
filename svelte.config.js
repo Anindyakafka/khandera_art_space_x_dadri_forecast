745c3dc2dd153889cb52a35654733d359c93c645
Anindya <73954190+Anindyakafka@users.noreply.github.com>
2026-03-16 17:44:58 +0530

    Commit static changes for neocities

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    prerender: {
+      default: true
+    },
     alias: {
       $lib: 'src/lib'
     }
